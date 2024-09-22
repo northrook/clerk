0a31d9cc7f232b84b960fde670ba6e3a8ed8ea78
Martin <hello@northrook.com>
2024-09-22 07:25:11 +0100

    mend

--- src/Clerk/ClerkEvent.php
+++ src/Clerk/ClerkEvent.php
@@ -7,12 +7,27 @@ use Symfony\Component\Stopwatch\Stopwatch;
 
 
 /**
+ *
+ * Represents a {@see Stopwatch} event started by {@see \Northrook\Clerk}.
+ *
+ * - Tracks execution time for a named event.
+ * - Can be categorized by group.
+ * - Can automatically start and lap when called via {@see \Northrook\Clerk::event()}.
+ *
  * @internal
+ *
  * @author Martin Nielsen <mn@northrook.com>
  */
 final readonly class ClerkEvent
 {
-
+    /**
+     * The event starts automatically when created by default.
+     *
+     * @param Stopwatch  $stopwatch
+     * @param string     $name
+     * @param ?string    $group
+     * @param bool       $autoStart
+     */
     public function __construct(
             private Stopwatch $stopwatch,
             public string     $name,
@@ -26,8 +41,12 @@ final readonly class ClerkEvent
     }
 
     /**
-     * @param ?string                     $log  language=Smarty
-     * @param array<string, \Stringable>  $context
+     * Start the stopwatch event, if it is not already active.
+     *
+     * Optionally logs a message.
+     *
+     * @param ?string                     $log      An optional log message.
+     * @param array<string, \Stringable>  $context  Optional context array for the log.
      *
      * @return void
      */
@@ -44,19 +63,37 @@ final readonly class ClerkEvent
         $this->stopwatch->start( $this->name, $this->group );
     }
 
+    /**
+     * Records a lap for the event if it is active. If not, starts the event.
+     *
+     * @return void
+     */
     public function lap() : void
     {
-        $this->stopwatch->lap( $this->name );
+        match ( $this->isActive() ) {
+            true  => $this->stopwatch->lap( $this->name ),
+            false => $this->start(),
+        };
     }
 
+    /**
+     * Stops the stopwatch event if it is currently active.
+     *
+     * @return void
+     */
     public function stop() : void
     {
-        if ( !$this->isActive() ) {
-            return;
+        if ( $this->isActive() ) {
+            $this->stopwatch->stop( $this->name );
         }
-        $this->stopwatch->stop( $this->name );
     }
 
+
+    /**
+     * Checks whether the stopwatch event is currently running.
+     *
+     * @return bool
+     */
     public function isActive() : bool
     {
         return $this->stopwatch->isStarted( $this->name );
